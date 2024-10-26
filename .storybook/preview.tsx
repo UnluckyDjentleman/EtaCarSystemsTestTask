89f16ad736885ecfb2bcc62084d4bb536f705604
dalvhikki <v.goroshchenja@gmail.com>
2024-10-26 12:07:39 +0300

    fix: fix storybooks

---
+++ .storybook/preview.tsx
@@ -0,0 +1,42 @@
+import type { Preview } from "@storybook/react";
+import { reactRouterParameters, withRouter } from 'storybook-addon-remix-react-router';
+import { Provider } from 'react-redux';
+import { ModalProvider } from "../src/components/modals/context/modal.context";
+import React from "react";
+import '../src/index.css'
+import {store} from '../src/store/store'; // Adjust the path to your store
+
+
+const preview: Preview = {
+  parameters: {
+      controls: {
+          matchers: {
+              color: /(background|color)$/i,
+              date: /Date$/i,
+          },
+      },
+      reactRouter: reactRouterParameters({
+          location: {
+              pathParams: {
+                  id: 'bitcoin',
+              },
+          },
+          routing: {
+              path: 'coins/:id',
+          },
+      }),
+  },
+  tags:['autodocs'],
+  decorators: [
+    (Story) => (
+      <Provider store={store}>
+        <ModalProvider>
+          <Story />
+        </ModalProvider>
+      </Provider>
+    ),
+    withRouter
+  ],
+};
+
+export default preview;
